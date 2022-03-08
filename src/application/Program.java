03539c41dd021d441f323b53a69db014ea675eb1
tatebout <tarcisio.coutinho@grupoicts.com.br>
2022-03-07 21:01:20 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import model.entity.Department;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Book");
+		System.out.println(obj);
+	}
+}
