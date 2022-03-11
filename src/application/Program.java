1363e7f3401af48152c3f5a4a861c59f94f04340
tatebout <tarcisio.coutinho@grupoicts.com.br>
2022-03-11 17:03:07 -0300

    tarcisio.coutinho@CTS-DF-RYB MINGW64 /d/Documentos/CursodeJavaUDEMY/Eclipse/demo-dao-jdbc (master) $ git commiReusing instantiationt - m

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,7 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entity.Department;
 import model.entity.Seller;
 
 public class Program {
@@ -12,8 +9,8 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 	}
 }
